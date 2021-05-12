9ce3693045f5789eb57bc8909d979e063da17434
jisu7487 <jisu7487@naet.com>
2021-05-12 10:03:04 +0900

    wait page

--- MyHandler.js
+++ MyHandler.js
@@ -1,16 +1,42 @@
 function start(res) {
-    let body = 'Hello, world! <br> I am in the cloud class.';
+  let body = '<head><meta charset ="UTF-8"/></head>';
+  body += '<body><div>Hello, world! <br> I am in the cloud class.</div><br>';
+  body += '<div><a href="/hello">hello 페이지</a></div>';
+  body += '<div><a href="/wait">5초 대기 페이지</a></div>';
+  body += '<div><a href="/randomWait">무작위 대기 페이지</a></div>';
+  body += '</body>';
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(body);
+  res.end();
+}
+
+function hello(res) {
+  let body = 'This is my first web server.';
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(body);
+  res.end();
+}
+
+function wait(res) {
+  setTimeout(function () {
+    let body = 'Thank you for waiting for 5 seconds.';
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(body);
     res.end();
-  }
-  
-  function hello(res) {
-    let body = 'This is my first web server.';
+  }, 5000);
+}
+
+function randomWait(res) {
+  let waitTime = Math.round(Math.random()*10000.);
+  setTimeout(function () {
+    let body = 'Thank you for waiting for ' + waitTime + ' ms.';
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(body);
     res.end();
-  }
-  
-  exports.start = start;
-  exports.hello = hello;
+  }, waitTime);
+}
+
+exports.start = start;
+exports.hello = hello;
+exports.wait = wait;
+exports.randomWait = randomWait;
--- upload.bat
+++ upload.bat
@@ -1,3 +1,4 @@
+git remote add origin https://github.com/jisu7487/2021new.git
 git add --all
-git commit -m "make MyHandler.js"
-git push -u origin master
+git commit -m "wait page"
+ git push -u origin master
