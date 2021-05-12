230038bf2cca1ea069520d91da2fab8f17bbd7b4
jisu7487 <jisu7487@naet.com>
2021-05-12 10:35:58 +0900

    add htmlFile function and firstHtml.html page

--- MyRouter.js
+++ MyRouter.js
@@ -1,14 +1,23 @@
-  function route(pathname, handle, res) {
-    console.log('Routing request for ' + pathname);
-    if (typeof handle[pathname] === 'function') {
-      handle[pathname](res);
+const fs = require('fs');
+const myHandler = require('./MyHandler');
+
+function route(pathname, handle, res) {
+  console.log('Routing request for ' + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](res);
+  } else {
+    pathFile = '.' + pathname + '.html'; // ex) ./page.html
+    if (fs.existsSync(pathFile)) {
+      console.log(pathFile + 'is found.');
+      myHandler.htmlFile(res, pathFile);
     } else {
-      console.log('No handler for'+ pathname);
-      let bodt = '404 Not Found';
+      console.log('No handler for ' + pathname);
+      let body = '404 Not Found';
       res.writeHead(404, { 'Content-Type': 'text/html' });
       res.write(body);
       res.end();
     }
   }
-  
-  exports.route = route;
+}
+
+exports.route = route;
